8b5e0d6a673a4bc5d54fe5ad64a30b2d6f595760
Rebeca-Reis <rebecapreis@gmail.com>
2017-04-20 11:54:15 -0300

    Add files via upload

---
+++ picm2017.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+//#include <koolplot.h>
+//#include <graphics.h>
+
+using namespace std;
+
+float theta,d,d0,y,v,tensao,deformacao_relativa;
+char[100] material;
+ 
+ int main(){
+ 	
+ }
