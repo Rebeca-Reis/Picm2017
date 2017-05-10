2c81f49058028ceff7b055d0a2767edbd87e9b0a
Rebeca-Reis <rebecapreis@gmail.com>
2017-05-09 21:32:17 -0300

    Add files via upload
    Corrigidos diversos bugs no banco de dados, e foi adicionada a opção do usuário digitar com vírgula e esta ser convertida para ponto.

--- picm.cpp
+++ picm.cpp
@@ -3,12 +3,14 @@
 #include <cmath>
 #include <cstdlib>
 using namespace std;
+#define pi 3.141592
 
 vector<Material> materiais;
-float dist[3][9],deformacao[3][9];
-float fi[3]={0,45,90},psi[9]={-65,-51.71,-39.86,-26.95,0,26.95,39.86,51.71,65};
-float sen2psi[9]={0.821,0.616,0.411,0.205,0,0.205,0.411,0.616,0.821};
-float m[3]; //coeficiente angular da reta para cada fi
+double dist[3][9],deformacao[3][9],sigma_fi[3],sigmas[3][2]; //sigmas: cada linha corresponde a um fi, e cada coluna é sigma11 ou sigma22
+double fi[3]={0,pi/4,pi/2};
+double psi[9]={-65,-51.71,-39.86,-26.95,0,26.95,39.86,51.71,65};
+double sen2psi[9]={0.821,0.616,0.411,0.205,0,0.205,0.411,0.616,0.821};
+double m[3]; //coeficiente angular da reta para cada fi
 int k;
 
 
@@ -19,21 +21,35 @@ bool buscaMaterial(string nome){
 }
 
 void criarMaterial(string n){
-	float y,p,d;
+	double y,p,d;
+	string virgula;
 	cout << "Vamos adicionar esse material a nosso banco de dados.\nInforme o módulo de Young para esse material: ";
-	cin >> y;
+	cin >> virgula;
+	replace(virgula.begin(),virgula.end(),',','.'); //converte o numero com virgula para ponto
+	y = atof(virgula.c_str());  //passa de string para double
+	virgula.erase();
 	cout << "Informe o coeficiente de poisson do material: ";
-	cin >> p;
+	cin >> virgula;
+	replace(virgula.begin(),virgula.end(),',','.'); //converte o numero com virgula para ponto
+	p = atof(virgula.c_str());  //passa de string para double
+	virgula.erase();
 	cout << "Informe a distância padrão d0 para esse material: ";
-	cin >> d;
+	cin >> virgula;
+	replace(virgula.begin(),virgula.end(),',','.'); //converte o numero com virgula para ponto
+	d = atof(virgula.c_str());  //passa de string para double
+	virgula.erase();
 	Material mat = Material(n,y,p,d);
 	materiais.push_back(mat);
 }
 
 void alteraMaterial(string n){
-	float d;
+	double d;
+	string virgula;
 	cout << "O material já consta no banco de dados.\nInforme o valor da distancia padrão d0: ";
-	cin >> d;
+	cin >> virgula;
+	replace(virgula.begin(),virgula.end(),',','.'); //converte o numero com virgula para ponto
+	d = atof(virgula.c_str());  //passa de string para double
+	virgula.erase();
 	for(int i=0; i<materiais.size();i++) if (materiais[i].getNome() == n and materiais[i].getD0() == 0) materiais[i].setD0(d);
 }
 
@@ -43,17 +59,17 @@ bool verifica_d0(string nome){
 	return resp; 
 }
 
-float lms(float *x, float deformacao[][9], int l) //REGRESSÃO LINEAR
+double lms(double *x, double deformacao[][9], int l) //REGRESSÃO LINEAR
 {
-    float y[9]; 
+    double y[9]; 
     for(int i=0;i<9;i++) y[i]=deformacao[l][i]; //l= 0,1 ou 2, depende do fi
     int i;
-    float m;
-    float sumYX = 0.;
-    float sumX = 0.;
-    float sumY = 0.;
-    float sumX2 = 0.;
-    float sum2X = 0.;
+    double m;
+    double sumYX = 0.;
+    double sumX = 0.;
+    double sumY = 0.;
+    double sumX2 = 0.;
+    double sum2X = 0.;
     for(i = 0; i < 9; i++){
         sumYX += x[i] * y[i];
         sumX += x[i];
@@ -62,25 +78,40 @@ float lms(float *x, float deformacao[][9], int l) //REGRESSÃO LINEAR
     }
     sum2X = sumX * sumX;
     m = (sumYX - (sumX * sumY) / 9.0) / (sumX2 - sum2X / 9.0);
-    //b = sumY / (float)n - *m * sumX / (float)n;
+    //b = sumY / (double)n - *m * sumX / (double)n;
     return m;
 } //Para chamar a funçao, use lms(sen2psi,deformacao,l))
 
 
 void realizarCalculos(string nome){
-	cout << "Oi! Vc está no caminho certo!" << endl;
+	string virgula;
 	memset(dist,0,sizeof(dist));
 	memset(deformacao,0,sizeof(deformacao));
 	cout << "Por favor, primeiramente insira os 9 valores para as distância entre os planos difratores inclinados referentes ao ângulo fi=0°\n";
 	cout << "Para isso, digite os valores correspondentes a cada psi em ordem crescente desses ângulos, apertando o botão ENTER entre cada um\n";
-	for(int i=0;i<9;i++) cin >> dist[0][i];
+	for(int i=0;i<9;i++){
+		cin >> virgula;
+		replace(virgula.begin(),virgula.end(),',','.'); //converte o numero com virgula para ponto
+		dist[0][i] = atof(virgula.c_str());  //passa de string para double
+		virgula.erase();
+	} 
 	cout << "Por favor, agora insira os 9 valores para as distância entre os planos difratores inclinados referentes ao ângulo fi=45°\n";
 	cout << "Para isso, digite os valores correspondentes a cada psi em ordem crescente desses ângulos, apertando o botão ENTER entre cada um\n";
-	for(int i=0;i<9;i++) cin >> dist[1][i];
+	for(int i=0;i<9;i++){
+		cin >> virgula;
+		replace(virgula.begin(),virgula.end(),',','.'); //converte o numero com virgula para ponto
+		dist[1][i] = atof(virgula.c_str());  //passa de string para double
+		virgula.erase();
+	}
 	cout << "Por favor, agora insira os 9 valores para as distância entre os planos difratores inclinados referentes ao ângulo fi=90°\n";
 	cout << "Para isso, digite os valores correspondentes a cada psi em ordem crescente desses ângulos, apertando o botão ENTER entre cada um\n";
-	for(int i=0;i<9;i++) cin >> dist[2][i];
-	//usuário já inseriu toas as informações requeridas 
+	for(int i=0;i<9;i++){
+		cin >> virgula;
+		replace(virgula.begin(),virgula.end(),',','.'); //converte o numero com virgula para ponto
+		dist[2][i] = atof(virgula.c_str());  //passa de string para double
+		virgula.erase();
+	} 
+	//usuário já inseriu todas as informações requeridas 
 	//vamos encontrar o índice(k) do material na lista do banco de dados
 	for (int i=0;i<materiais.size();i++) if (nome == materiais[i].getNome()) k=i;
 	//preenchimento da matriz das deformações
@@ -90,12 +121,29 @@ void realizarCalculos(string nome){
 		}
 	}
 	for(int i=0;i<3;i++) m[i]=lms(sen2psi,deformacao,i);
-	
+	printf("m1: %lf\nm2: %lf\nm3: %lf\n",m[0],m[1],m[2]);
+	double y_mat = materiais[k].getYoung(); //unidade: GPa
+	double p_mat = materiais[k].getPoisson();
+	for(int i=0;i<3;i++) sigma_fi[i]=m[i]*(y_mat*1000)/(1+p_mat); //unidade: MPa
+	for(int i=0;i<3;i++) printf("sigma_fi: %f MPa\n",sigma_fi[i]);	
+	for(int i=0;i<3;i++){ //calculo de sigma11 e sigma22 : unidade MPa
+		if(i==1) continue; //para fi=45º o denominador se anula, por isso nao calcularemos sigma11 e sigma22 para este fi
+		sigmas[i][0]=sigma_fi[i] + (1000*y_mat*deformacao[i][4]*sin(fi[i])*sin(fi[i]))/p_mat;
+		sigmas[i][0] /= (cos(fi[i])*cos(fi[i]) - sin(fi[i])*sin(fi[i]));
+		sigmas[i][1]=-(sigma_fi[i] + (1000*y_mat*deformacao[i][4]*cos(fi[i])*cos(fi[i]))/p_mat);
+		sigmas[i][1] /= (cos(fi[i])*cos(fi[i]) - sin(fi[i])*sin(fi[i]));
+	}	
+	printf("Sigmas 11: %lf\n\t%lf\n\t%lf\nsigmas22: %lf\n\t%lf\n\t%lf\n",sigmas[0][0],sigmas[1][0],sigmas[2][0],sigmas[0][1],sigmas[1][1],sigmas[2][1]);
+	double media_11, media_22;
+	media_11=(sigmas[0][0] + sigmas[2][0])/2; //consertar as médias, talvez não faça sentido pra teoria
+	media_22=(sigmas[0][1] + sigmas[2][1])/2;
+	cout << "Fazendo as médias, obtemos:\nsigma11= " << media_11 << "\nsigma22= " << media_22 << endl;
 }
 
 void menu(){
 	string nome;
 	int op;
+	cout << "O que vc deseja fazer?\n";
 	cout << "\nDigite:\n1 - Consulta\n2 - Adicionar Material\n3 - Sair do Programa\n\n";
 	cin >> op;
 	switch(op){
@@ -131,9 +179,8 @@ int main(){
 
 	Material mat;
 	string s;
-	float n1,n2,n3;
-	cout << "Seja bem vindo ao nosso Software de Cálculo de Tensão Residual :-) \nO que vc deseja fazer?\n";
-
+	double n1,n2,n3;
+	cout << "Seja bem vindo ao nosso Software de Cálculo de Tensão Residual :-)\n";
 	// PRIMEIRA COISA DO MAIN
 	fstream file("Registro.txt");
 	if(file.is_open()){
@@ -144,6 +191,8 @@ int main(){
 	}
 	file.close();
 
+
+
 	menu();
 
 	// ULTIMA COISA DO MAIN
@@ -151,5 +200,5 @@ int main(){
 	if(novo.is_open()){
 		for(int i=0;i<materiais.size();i++) novo << materiais[i].getNome() << " " << materiais[i].getYoung() << " " << materiais[i].getPoisson() << " " << materiais[i].getD0() << endl;
 	}
-	novo.close();
+	novo.close();	
 }
